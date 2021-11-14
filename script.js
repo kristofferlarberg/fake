69548740f8707e4ea408a11a3f493cd1bab7cb9f
kristofferlarberg <mail@kristofferlarberg.se>
2021-11-14 22:29:14 +0100

    Limit width for repeatWave function and add end to pattern squares

--- script.js
+++ script.js
@@ -24,19 +24,37 @@ const invertedWaveArr = [
   [-10, 0],
 ];
 
+const endWaveArr = [
+  [-100, 30],
+  [-90, 30],
+  [-90, 30],
+  [-80, 20],
+  [-70, 10],
+  [-70, 0],
+  [-70, -10],
+  [-80, -20],
+  [-90, -30],
+  [-100, -30],
+
+
+];
+
 const topWaveArr = [
   [-80, 20],
   [-70, 20],
   [-60, 30],
   [-50, 40],
   [-50, 50],
-  [-50, 0],
-  [-40, 10],
   [-40, 40],
   [-40, 50],
   [-30, 30],
   [-20, 20],
   [-10, 20],
+
+
+  [-50, 20],
+  [-30, 0],
+  [-40, 10],
 ];
 
 function createWave(offsetX, offsetY, arr) {
@@ -47,39 +65,42 @@ function createWave(offsetX, offsetY, arr) {
   }
 }
 
-function repeatWave(offsetY, arr) {
+function repeatWave(offsetY, arr, amount, end) {
   let x = 0;
   let y = offsetY;
-  for (let i = 0; i < width; i += 80) {
-    for (let i = 0; i < 3; i++) {
-      x += 80;
-      createWave(x, y, arr)
-    }
-    x += 90;
-    createWave(x, y, invertedWaveArr)
-    x += 10;
+  for (let i = 0; i < amount; i++) {
+    x += 80;
+    createWave(x, y, arr)
   }
+  x += 100;
+  if (end) createWave(x, y , endWaveArr);
 }
 
 let y1 = -35;
 let y2 = -30;
 
+// Make possible to x repeat
+
 function createPattern() {
   for (let i = 0; i < height; i += 40) {
-    for (let i = 0; i < 5; i++) {
+    console.log(i)
+    for (let i = 0; i < 3; i++) {
       y1 += 35;
       y2 += 35;
-      repeatWave(y1, waveArr)
-      repeatWave(y2, invertedWaveArr)
+      repeatWave(y1, waveArr, 3, true)
+      y1 += 10;
+      y2 += 10;
+      repeatWave(y2, invertedWaveArr, 3, false)
     }
     y1 += 35;
     y2 += 35;
-    repeatWave(y1, waveArr)
-    y1 += 45;
-    y2 += 45;
-    repeatWave(y2, topWaveArr)
-    y1 += 20;
-    y2 += 20;
+    repeatWave(y1, waveArr, 3, false)
+    y1 += 35;
+    y2 += 35;
+
+    repeatWave(y2, topWaveArr, 3, false)
+    y1 += 10;
+    y2 += 10;
   }
 }
 
