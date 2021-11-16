5c450692af38070053a79237cf595cf3a806bf0b
kristofferlarberg <mail@kristofferlarberg.se>
2021-11-16 22:47:33 +0100

    In the middle of refactoring

--- script.js
+++ script.js
@@ -25,22 +25,24 @@ const invertedWaveArr = [
 ];
 
 const endWaveArr = [
-  [-100, 30],
-  [-90, 30],
-  [-90, 30],
-  [-80, 20],
+  [-100, 8],
+  [-90, 5],
+  [-80, 26],
   [-70, 10],
-  [-70, 0],
-  [-70, -10],
-  [-80, -20],
-  [-90, -30],
-  [-100, -30],
+];
 
-
+const bottomWaveArr = [
+  [-70, 30],
+  [-60, 20],
+  [-50, 0],
+  [-50, 10],
+  [-40, 0],
+  [-40, 10],
+  [-30, 20],
+  [-20, 30],
 ];
 
 const topWaveArr = [
-  [-80, 20],
   [-70, 20],
   [-60, 30],
   [-50, 40],
@@ -49,56 +51,56 @@ const topWaveArr = [
   [-40, 50],
   [-30, 30],
   [-20, 20],
-  [-10, 20],
-
-
-  [-50, 20],
-  [-30, 0],
-  [-40, 10],
 ];
 
 function createWave(offsetX, offsetY, arr) {
+  let fade = 0;
   for (let i = 0; i < arr.length; i++) {
     let x = arr[i][0] + offsetX;
     let y = arr[i][1] + offsetY;
-    rect(x, y, 10)
+    for (let j = 0; j < 255; j += 1) {
+      fill(0, 0, 255, fade)
+      if (fade < 0) fadeAmount = 1;
+      fade += j;
+      console.log(fade)
+      rect(x, y, 10)
+    }
   }
 }
 
-function repeatWave(offsetY, arr, amount, end) {
-  let x = 0;
-  let y = offsetY;
+function repeatWave(offsetX, offsetY, arr, amount, end) {
+  let x = offsetX;
   for (let i = 0; i < amount; i++) {
     x += 80;
-    createWave(x, y, arr)
+    createWave(x, offsetY, arr, fill)
   }
   x += 100;
-  if (end) createWave(x, y , endWaveArr);
+  if (end) createWave(x, offsetY, endWaveArr, fill);
 }
 
 let y1 = -35;
 let y2 = -30;
 
-// Make possible to x repeat
 
-function createPattern() {
+function createPattern(offsetX) {
+  console.log("offsetX")
+  console.log(offsetX)
+
   for (let i = 0; i < height; i += 40) {
-    console.log(i)
     for (let i = 0; i < 3; i++) {
       y1 += 35;
       y2 += 35;
-      repeatWave(y1, waveArr, 3, true)
-      y1 += 10;
-      y2 += 10;
-      repeatWave(y2, invertedWaveArr, 3, false)
+      repeatWave(offsetX, y1, waveArr, 3, true)
+      y1 += 0;
+      y2 += 0;
+      repeatWave(offsetX, y2, invertedWaveArr, 3, false)
     }
-    y1 += 35;
-    y2 += 35;
-    repeatWave(y1, waveArr, 3, false)
-    y1 += 35;
-    y2 += 35;
-
-    repeatWave(y2, topWaveArr, 3, false)
+    y1 += 25;
+    y2 += 25;
+    repeatWave(offsetX, y1, bottomWaveArr, 3, false)
+    y1 += 5;
+    y2 += 5;
+    repeatWave(offsetX, y2, topWaveArr, 3, false)
     y1 += 10;
     y2 += 10;
   }
@@ -108,14 +110,24 @@ function setup() {
   const canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('container');
   background(0);
-  fill(190);
+  fade = 0
 }
-
 
 function draw() {
   noStroke();
 
-  createPattern()
+
+
+
+  // createPattern(0)
+
+  createWave(100, 100, waveArr);
+
+  // let y1 = -35;
+  // let y2 = -30;
+
+
+
 
   noLoop();
 }
